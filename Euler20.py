ab3fc8b02b9e77d10d92f37a8df28d9126ceced1
Andrew Voss <avoss19@bsmschool.org>
2017-09-22 13:49:33 -0500

    created & finished Euler20

---
+++ Euler20.py
@@ -0,0 +1,17 @@
+# Euler 20
+# https://projecteuler.net/problem=20
+# Created by Andrew Voss
+
+# main variables
+factor = 100 # factorial number
+x = 1
+total = 1
+
+while x <= 100:
+     total = total * x
+     x += 1
+
+x = str(total)
+x = (list(x))
+x = map(int, x)
+print sum(x)
