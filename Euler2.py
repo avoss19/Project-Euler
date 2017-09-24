c17f18097bd273eaa137e3b0c8b0b941c19f0756
Andrew Voss <voss1167@gmail.com>
2017-09-24 16:31:57 -0500

    wft

--- Euler2.py
+++ Euler2.py
@@ -3,16 +3,18 @@
 # Created by Andrew Voss
 
 # define variables
+MAX = 4e6 # Fibonacci max value
 x1 = 1
 x2 = 2
 temp = 0
 total = 0
-MAX = 4e6 # Max value
 
 # Fibonacci
 while x1 < MAX:
+    # add even number fibonacci
     if x1 % 2 == 0:
         total += x1
+    # add next fibonacci number
     temp = x1
     x1 = x2
     x2 = x2 + temp
