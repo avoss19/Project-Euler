97fcbb12f9c5c3865fe626c6814a44ef99a2e980
Andrew Voss <voss1167@gmail.com>
2017-11-26 11:50:57 -0600

    fin

---
+++ Euler3.py
@@ -0,0 +1,13 @@
+# Euler 3
+# https://projecteuler.net/problem=3
+# Created by Andrew Voss
+
+# Define Variables
+factor = 600851475143 # Prime Numbers Of
+
+i = 2
+while i * i < factor:
+    while factor % i == 0:
+        factor = factor / i
+    i = i + 1
+print factor
