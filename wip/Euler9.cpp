4777e6b2303bf94e3358639799b6ec50b3e905f9
Andrew Voss <voss1167@gmail.com>
2017-11-26 20:02:24 -0600

    wip

---
+++ wip/Euler9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+bool checkSum1000(int x,int y,int z){
+  if(x+y+z==1000)
+    return true;
+  else
+    return false;
+}
+
+int testing(int x){
+  return (x);
+}
+
+
+int main()
+{
+  cout << testing(1) << endl;
+}
